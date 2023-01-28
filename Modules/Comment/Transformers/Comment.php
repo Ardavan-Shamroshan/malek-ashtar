43f9c0b6ef10a60990db6d4260f57c817cf8da8c
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2023-01-28 21:38:20 +0330

    ApiPostCategoryController modified - Post, Comment, PostCategory resource in progress

---
+++ Modules/Comment/Transformers/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Comment\Transformers;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Comment extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
---
+++ Modules/PostCategory/Http/Controllers/ApiPostCategoryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Modules\PostCategory\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use Modules\PostCategory\Entities\PostCategory;
+use Modules\PostCategory\Http\Requests\PostCategoryRequest;
+
+class ApiPostCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index() {
+        // return 10 post categories per page where has active status (status = 1)
+        return PostCategory::query()->status()->paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request) {
+        dd('api/store');
+        $inputs = $request->all();
+        return PostCategory::query()->create($inputs);
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Response
+     */
+    public function show($id) {
+        return PostCategory::query()->findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(Request $request, $id) {
+
+        $inputs = $request->all();
+        $postcategory = PostCategory::query()->findOrFail($id);
+        return $postcategory->update($inputs);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Response
+     */
+    public function destroy($id) {
+        dd('api/destroy');
+        $postcategory = PostCategory::query()->findOrFail($id)->delete();
+    }
+}
